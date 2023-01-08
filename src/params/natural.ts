4497b377736c3827c1b48431d691380487069c05
Zsong Lee <zsongli@gmail.com>
2023-01-08 02:07:04 +0100

    clarify param matcher naming

--- src/params/natural.ts
+++ src/params/natural.ts
@@ -1,3 +1,4 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
+// this param matcher doensn't allow negative numbers
 export const match = (param => /^\d+$/.test(param)) satisfies ParamMatcher;
