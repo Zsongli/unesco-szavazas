b0ba9b0b2f0135398f21a8aee1d8cda5ffed3cb6
Zsong Lee <zsongli@gmail.com>
2023-01-27 00:24:41 +0100

    fix bugs when lag occurs and add a column for indices

--- src/lib/components/OrderableList.svelte
+++ src/lib/components/OrderableList.svelte
@@ -8,38 +8,34 @@
 
 	export let items: T[] = [];
 	export let display: (item: T) => object = (item) => item;
-	const animationDuration = 200;
 
 	var rows: HTMLDivElement[] = [];
+	var rowRects: DOMRect[] = [];
 	var container: HTMLTableSectionElement | null = null;
+	var containerRect: DOMRect | null = null;
+	$: updateRects(rows, container);
+	function updateRects(...args: any[]) {
+		rowRects = rows.map((row) => row.getBoundingClientRect());
+		containerRect = container?.getBoundingClientRect() ?? null;
+	}
 
-	$: containerRect = container?.getBoundingClientRect();
-	$: rowRects = rows.map((row) => row.getBoundingClientRect());
-
-	var controlsDisabled = false;
 	var draggedIndex = -1;
-	var dragStartPos = 0;
-	var offset = 0;
 	var dropBeforeIndex = -1;
 
+	var dragStartPos = 0;
+	var offset = 0;
+
 	function dragstart(i: number, e: PointerEvent) {
-		if (draggedIndex !== -1 || controlsDisabled) return;
-		document.body.style.overflow = "hidden";
+		if (draggedIndex !== -1) return;
+		updateRects();
+
 		offset = 0;
 		draggedIndex = i;
 		dragStartPos = e.clientY;
 	}
 
-	function dragmouse(e: MouseEvent) {
-		drag(e.clientY);
-	}
-
-	function dragtouch(e: TouchEvent) {
-		drag(e.touches[0].clientY);
-	}
-
 	function drag(pointerY: number) {
-		if (draggedIndex == -1) return;
+		if (draggedIndex === -1) return;
 
 		var futureOffset = pointerY - dragStartPos;
 
@@ -58,7 +54,7 @@
 		);
 
 		offset = futureOffset;
-	}	
+	}
 
 	function dragend() {
 		if (dropBeforeIndex !== -1) {
@@ -66,23 +62,24 @@
 				dropBeforeIndex < draggedIndex ? dropBeforeIndex : dropBeforeIndex - 1,
 				0,
 				items.splice(draggedIndex, 1)[0]
-			);
-			items = items;
-			controlsDisabled = true;
-			setTimeout(() => {
-				rowRects = rows.map((row) => row.getBoundingClientRect());
-				controlsDisabled = false;
-			}, animationDuration + 1);
+			); // move item to new position
+			items = items; // trigger update
 			dropBeforeIndex = -1;
 		}
+
 		draggedIndex = -1;
-		document.body.style.overflow = "auto";
+	}
+
+	const pointermove = (e: PointerEvent) => drag(e.clientY);
+	function touchmove(e: TouchEvent) {
+		if (draggedIndex !== -1) e.preventDefault();
+		drag(e.touches[0].clientY);
 	}
 </script>
 
 <svelte:window
-	on:pointermove={dragmouse}
-	on:touchmove={dragtouch}
+	on:pointermove={pointermove}
+	on:touchmove|nonpassive={touchmove}
 	on:pointerup={dragend}
 	on:touchend={dragend}
 />
@@ -90,6 +87,11 @@
 <div class="relative overflow-x-auto overflow-y-hidden {$$restProps.class}">
 	<table class="w-full text-left text-sm text-gray-500 dark:text-gray-400 h-full">
 		<TableHead class="text-gray-700 dark:text-gray-400 bg-gray-50 dark:bg-gray-700">
+			{#if $$slots["index-col"]}
+				<th class="px-6 py-3 w-0 border-r border-gray-800 hidden sm:table-cell">
+					<slot name="index-col-header" />
+				</th>
+			{/if}
 			{#each Object.keys(display(items[0])) as key}
 				<TableHeadCell>{key}</TableHeadCell>
 			{/each}
@@ -101,22 +103,35 @@
 			{#each items as item, i (item.id)}
 				<tr
 					animate:flip={{
-						duration: $navigating == null && draggedIndex === -1 ? animationDuration : 0
+						duration: $navigating == null && draggedIndex === -1 ? 200 : 0
 					}}
-					class="transition-opacity border-b last:border-b-0 bg-white dark:bg-gray-800 {dropBeforeIndex !==
+					class="transition-opacity border-b last:border-b-0 bg-white dark:border-gray-700 dark:bg-gray-800 dark:outline-gray-700 outline {draggedIndex ===
 					i
-						? 'dark:border-gray-700'
-						: 'border-transparent'}"
-					style="{draggedIndex === i
-						? `translate: 0 ${offset}px; --tw-bg-opacity: 0.75; outline: 1px solid rgb(55 65 81 / .75);`
-						: ''}
-						{dropBeforeIndex !== -1 && dropBeforeIndex === i ? 'box-shadow: 0 -4px 0 0 rgb(28 100 242);' : ''}"
+						? '!bg-opacity-50 outline-1'
+						: 'border-opacity-100 bg-opacity-100 outline-0'}"
+					style={draggedIndex === i ? `translate: 0 ${offset}px;` : ""}
 					bind:this={rows[i]}
 				>
+					{#if $$slots["index-col"]}
+						<TableBodyCell class="border-r border-gray-700 hidden sm:table-cell">
+							<slot name="index-col" index={i} />
+						</TableBodyCell>
+					{/if}
+
 					{#each Object.values(display(item)) as value}
-						<TableBodyCell>{value}</TableBodyCell>
+						<TableBodyCell
+							style={dropBeforeIndex !== -1 && dropBeforeIndex === i
+								? "box-shadow: 0 -4px 0 0 rgb(28 100 242);"
+								: ""}>{value}</TableBodyCell
+						>
 					{/each}
-					<TableBodyCell class="flex items-center justify-end">
+
+					<TableBodyCell
+						class="flex items-center justify-end"
+						style={dropBeforeIndex !== -1 && dropBeforeIndex === i
+							? "box-shadow: 0 -4px 0 0 rgb(28 100 242);"
+							: ""}
+					>
 						<button class="cursor-grab flex items-center" on:pointerdown={(e) => dragstart(i, e)}>
 							<FasGripLines class="text-gray-500" />
 						</button>
