d95be0d34535c9ddb19964a9e7db5888637f36b9
Zsong Lee <zsongli@gmail.com>
2023-01-28 22:41:30 +0100

    add prototype for summary page

--- src/routes/results/+page.server.ts
+++ src/routes/results/+page.server.ts
@@ -1,5 +1,11 @@
+import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load = (async ({})=>{
-    return {};
+export const load = (async ({ locals }) => {
+    if (!locals.session) throw error(401);
+    if (!locals.session.user.role.permissions.includes("view-results")) throw error(403);
+
+    return {
+
+    };
 }) satisfies PageServerLoad;
--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -1,3 +1,45 @@
+<script lang="ts">
+	import { Span, Table, TableBody, TableBodyCell, TableBodyRow, TableHead } from "flowbite-svelte";
+	import type { PageData } from "./$types";
+	import FasSquarePollVertical from "~icons/fa6-solid/square-poll-vertical";
+
+	export var data: PageData;
+    console.log(data);
+</script>
+
 <svelte:head>
-    <title>Eredmények • Összesítés • UNESCO Szavazás</title>
+	<title>Eredmények • Összesítés • UNESCO Szavazás</title>
 </svelte:head>
+
+<Table divClass="relative overflow-x-auto rounded-t-lg" class="w-full">
+	<TableHead>
+		<th class="px-3 py-6 flex flex-col items-center justify-center gap-2">
+			<FasSquarePollVertical class="text-base" />
+			<Span class="text-center normal-case">Összesítés</Span>
+		</th>
+		{#each data.categories as category}
+			<th class="py-3 text-vertical rotate-[179.9deg] border-gray-800 border-l">{category}</th>
+		{/each}
+	</TableHead>
+	<TableBody>
+		{#each data.classes as { name, country }, i}
+			<TableBodyRow class="border-gray-600 divide-x">
+				<TableBodyCell>
+					<div class="flex flex-col items-center">
+						<div>{name}</div>
+						<div class="text-xs">{country}</div>
+					</div>
+				</TableBodyCell>
+				<!-- {#each data.tableData[i] as sum}
+					<TableBodyCell class="border-gray-700 text-center">{sum ?? ".."}.</TableBodyCell>
+				{/each} -->
+			</TableBodyRow>
+		{/each}
+	</TableBody>
+</Table>
+
+<style lang="postcss">
+    .text-vertical {
+        writing-mode: vertical-lr;
+    }
+</style>
