62bff561ebf52756a6bf90ad30b86b87ab0148b7
Beni <73666440+beni69@users.noreply.github.com>
2024-11-10 01:39:18 +0100

    update image carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,15 @@
 	export var data: PageData;
 
 	const photos = [
-		"spanyol5.jpg",
-		"ciprus2.jpg",
-		"egyiptom4.jpg",
-		"konferansz1.jpg",
-		"kina2.jpg",
-		"egyiptom0.jpg"
+        "golyaB.jpg",
+        "golyaE.jpg",
+        "golyaE2.jpg",
+        "golyaF.jpg",
+        "golyaF2.jpg",
+        "golyaMIB.jpg",
+        "golyaShrek.jpg",
+        "golyaTuz.jpg",
+        "beni.jpg",
 	];
 </script>
 
@@ -75,7 +78,7 @@
 			{/if}
 		</div>
 		<a
-			href="https://szlgbp.hu/2022/09/12/unesco-musor-2022/"
+			href="https://flic.kr/s/aHBqjB5q3x"
 			rel="noreferrer"
 			target="_blank"
 			class="hidden xl:block justify-self-end w-[420px] h-[420px] rounded-2xl overflow-hidden shadow-2xl"
@@ -83,7 +86,7 @@
 			<img
 				class="w-full h-full object-cover object-center"
 				src="/homepage/{photos[Math.floor(Math.random() * photos.length)]}"
-				alt="UNESCO Műsor 2022"
+				alt="Gólyabál 2023"
 			/>
 		</a>
 	</div>
